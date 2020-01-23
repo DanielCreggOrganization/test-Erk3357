78127c92bb13b6ae6aa02fa8cf9684bbf95e1d30
Eryk Kowalewicz <eryk.kowalewicz@outlook.com>
2020-01-23 14:40:10 +0000

    Week 1 - Lab 2

---
+++ src/ie/gmit/packagename/MyClass.java
@@ -0,0 +1,15 @@
+package ie.gmit.packagename;
+
+public class MyClass{
+    public static void main(String[] args) {
+        for (int i=1; i <= 10; i++){
+            myMethod();
+        }
+    }
+
+    public static void myMethod(){
+        System.out.println("I just got excecuted");
+    }
+}
+
+
